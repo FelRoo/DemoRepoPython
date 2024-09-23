4492c2d4b3a30cd8ebaef91a22fe25ebd63d397b
Felix <rooke.felix@gmail.com>
2024-09-23 15:56:53 +0200

    Kodexempel

---
+++ Kod0923/Constans.py
@@ -0,0 +1,11 @@
+from typing import Final
+
+# Constant variable
+
+PI:Final = 3.14
+
+PI = 3.1415926
+
+print(PI) 
+
+# Notera att Final inte är nödvändigt, men det är "best practice" att använda för konstanter för att göra koden mer lättläst och lättare att förstå.
---
+++ Kod0923/DataTypes.py
@@ -0,0 +1,22 @@
+# Description: Denna fil innehåller olika datatyper i Python.
+
+mitt_namn = "felix rooke"
+
+min_ålder = 16
+
+min_längd = 1.75
+
+jag_är_lärare = True
+
+ett_komplext_nummer = 1 + 5j
+
+
+#Strängar kan skriovas med både enkla och dubbla citattecken. Var konsekvent! Tänk på att om du använder enkla citattecken så kan du inte använda enkla citattecken inuti strängen utan att använda escape-tecken. Exempelvis: 'I\'m a teacher'
+
+min_stad = "Stockholm"
+
+mitt_land = 'Sweden'
+
+felaktig_sträng_ett = 'I'm a teacher'
+
+felaktig_sträng_två = "Jag heter Felix'
